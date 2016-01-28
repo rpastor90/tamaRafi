e38a75893dae14750b834d9c27e0114007870936
wnmprk <wonmipark@gmail.com>
2016-01-28 16:28:01 -0500

    formatting stuff

--- browser/js/login/login.js
+++ browser/js/login/login.js
@@ -13,13 +13,13 @@ app.controller('LoginCtrl', function ($scope, AuthService, $state) {
     $scope.login = {};
     $scope.error = null;
 
-    $scope.sendLogin = function (loginInfo) {
+    $scope.sendLogin = function(loginInfo) {
 
         $scope.error = null;
 
-        AuthService.login(loginInfo).then(function () {
+        AuthService.login(loginInfo).then(function() {
             $state.go('home');
-        }).catch(function () {
+        }).catch(function() {
             $scope.error = 'Invalid login credentials.';
         });
 
