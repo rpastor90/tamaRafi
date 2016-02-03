22887b61e955cd6670e30c81c7f95db780cf3ac9
Katherine Mello <katherinemello@Katherines-MacBook-Pro.local>
2016-02-02 20:24:48 -0500

    drag and drop functions

--- browser/js/crib/crib.js
+++ browser/js/crib/crib.js
@@ -27,10 +27,16 @@ app.factory('CribFactory', function () {
 });
 
 app.controller('CribCtrl', function ($scope, $state, user, AuthService, swags, AnimalFactory) {
+    // var tracker2 = function() {
+    //     var obj = document.getElementById("beingMoved")
+      
+    //     console.log(obj.offsetTop, obj.scrollLeft, obj.clientLeft)
+    // };
+
     $scope.user = user;
     $scope.isShown = false;
     $scope.swags = swags;
-    $scope.toggle = function9
+    // $scope.toggle = function9
     $scope.logout = function () {
         AuthService.logout()
         .then(function () {
@@ -38,9 +44,27 @@ app.controller('CribCtrl', function ($scope, $state, user, AuthService, swags, A
         });
     };
 
+    $scope.onStart = function () {
+        console.log( document.getElementById("beingMoved").offsetLeft)
+        console.log("you started!")
+    }
+
+    $scope.tracker = function(input) {
+        console.log(this, "this is the tracker function ")
+        var obj = document.getElementById("beingMoved")
+      
+        console.log(obj.offsetTop, obj.scrollLeft, obj.clientLeft)
+    };
+    
     $scope.startCallback = function() {
         console.log("hello");
+
     };
+    $scope.getCoords = function() {
+        $('#beingMoved').draggable()
+        console.log(this, "THIS")
+        console.log(arguments, "ARGS")
+    }
     
    
 });
